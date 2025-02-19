1c7911d331f5be6b07f39c1036a28396bf7e2bea
Spencer Perley <spencerp11338@gmail.com>
2025-02-19 00:43:04 -0800

    added the start screen

---
+++ Assets/menuese.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuese : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void LoadMyScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
